08c1e5867c0f587f4a6e7423f5ff7871616a2c79
milka_deluxe <milagalko1@gmail.com>
2024-03-01 19:11:41 +0200

    number generation + gameIsOverHandler + boundaries logic

--- GuessNumberApp/screens/GuessScreen.js
+++ GuessNumberApp/screens/GuessScreen.js
@@ -1,34 +1,41 @@
-import React, {useState} from 'react';
-import {StyleSheet, Text, View} from "react-native";
+import React, {useEffect, useState} from 'react';
+import {Alert, StyleSheet, Text, View} from "react-native";
 import BorderedTitle from "../components/BorderedTitle";
 import {Colors} from "../Colors";
-import StandardButton from "../components/buttons/StandardButton";
 import GuessList from "../components/guess/GuessList";
-import InputBlock from "../components/input/InputBlock";
 import LowerHigherBlock from "../components/input/LowerHigherBlock";
 
-const GuessScreen = () => {
-    const [guessNumbers, setGuessNumbers] = useState([Math.floor(Math.random() * 100) + 1]);
-    const [currentLow, setCurrentLow] = useState(1);
-    const [currentHigh, setCurrentHigh] = useState(100);
-    const lowerHandler = () => {
-        const currentNumber = guessNumbers[0];
-        if(currentNumber < currentHigh){
-            setCurrentHigh(currentNumber);
-        }
-        TryToGuess(currentLow, currentHigh);
-    }
-    const higherHandler = () => {
-        const currentNumber = guessNumbers[0];
-        if(currentNumber > currentLow){
-            setCurrentLow(currentNumber);
-        }
-        TryToGuess(currentLow, currentHigh);
-    }
+let minBound = 1;
+const GenerateNumber = (min, max) => {
+    let number = Math.floor(Math.random() * (max - min)) + min;
+    return number === min ? number + 1 : number;
+};
 
-    const TryToGuess = (currentLow, currentHigh) => {
-        const number = Math.floor((currentHigh - currentLow) / 2) + currentLow;
-        setGuessNumbers((currentGuessNumbers) => [number, ...currentGuessNumbers]);
+let maxBound = 100;
+
+const GuessScreen = ({userNumber, onGameOver}) => {
+    const [guessNumbers, setGuessNumbers] = useState([Math.floor(Math.random() * 100) + 1]);
+
+    useEffect(() => {
+        if(guessNumbers[0] === userNumber){
+            onGameOver();
+        }
+    }, [guessNumbers, userNumber, onGameOver]);
+
+
+    const guessHandler = (direction) => { // direction is 'lower' or 'higher'
+        if((direction === 'lower' && guessNumbers[0] < userNumber) ||
+            (direction === 'higher' && guessNumbers[0] > userNumber)){
+            Alert.alert(
+                "Don't lie!",
+                "You know that this is wrong...",
+                [{text: 'Sorry!', style: 'cancel'}]
+            );
+            return;
+        }
+
+        direction === 'lower' ? maxBound = guessNumbers[0] : minBound = guessNumbers[0];
+        setGuessNumbers((number) => [GenerateNumber(minBound, maxBound), ...number]);
     }
 
     return (
@@ -37,7 +44,9 @@ const GuessScreen = () => {
             <View style={styles.guessContainer}>
                 <Text style={styles.guessText}>{guessNumbers[0]}</Text>
             </View>
-            <LowerHigherBlock lowerHandler={lowerHandler} higherHandler={higherHandler}/>
+            <LowerHigherBlock
+                onLower={guessHandler.bind(this, 'lower')}
+                onHigher={guessHandler.bind(this, 'higher')}/>
             <GuessList guessNumbers={guessNumbers}/>
         </View>
     );
