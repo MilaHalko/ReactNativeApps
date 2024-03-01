16b093fae853abe6cb612794c2e79d728f27a9f1
milka_deluxe <milagalko1@gmail.com>
2024-03-01 22:08:41 +0200

    add: expo icons

--- GuessNumberApp/components/input/LowerHigherBlock.js
+++ GuessNumberApp/components/input/LowerHigherBlock.js
@@ -3,13 +3,15 @@ import {StyleSheet} from "react-native";
 import StandardButton from "../buttons/StandardButton";
 import InputBlock from "./InputBlock";
 import ButtonsContainer from "../buttons/ButtonsContainer";
+import {AntDesign} from "@expo/vector-icons";
+import {Colors} from "../../Colors";
 
 const LowerHigherBlock = (props) => {
     return (
         <InputBlock title="Higher or Lower ?" style={styles.inputBlockContainer}>
             <ButtonsContainer>
-                <StandardButton title="LOWER" onPress={props.onLower}/>
-                <StandardButton title="HIGHER" onPress={props.onHigher}/>
+                <StandardButton title={<AntDesign name="arrowdown" size={24} color={Colors.white}/>} onPress={props.onLower}/>
+                <StandardButton title={<AntDesign name="arrowup" size={24} color={Colors.white}/>} onPress={props.onHigher}/>
             </ButtonsContainer>
         </InputBlock>
     );
