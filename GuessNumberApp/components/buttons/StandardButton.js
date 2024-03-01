b06633e11f73eb113f80bcc762371721e7572dbe
milka_deluxe <milagalko1@gmail.com>
2024-03-01 19:04:30 +0200

    primary button component

--- GuessNumberApp/components/buttons/StandardButton.js
+++ GuessNumberApp/components/buttons/StandardButton.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import {Button, View, StyleSheet} from "react-native";
+import {Pressable, StyleSheet, Text, View} from "react-native";
 import {Colors} from "../../Colors";
 
 const StandardButton = (props) => {
     return (
-        <View style={styles.button}>
-            <Button title={props.title} onPress={props.onPress} color={Colors.yellow}/>
+        <View style={styles.buttonOuterContainer}>
+            <Pressable onPress={props.onPress}
+                       style={({pressed}) =>
+                           pressed ?
+                               [styles.buttonInnerContainer, pressed]
+                               : styles.buttonInnerContainer}
+                       android_ripple={{color: Colors.green}}>
+                <Text style={styles.text}>{props.title}</Text>
+            </Pressable>
+            {/*<Button title={props.title} onPress={props.onPress} color={Colors.yellow}/>*/}
         </View>
     );
 };
@@ -13,8 +21,28 @@ const StandardButton = (props) => {
 export default StandardButton;
 
 const styles = StyleSheet.create({
-    button: {
-        borderRadius: 25,
+    buttonOuterContainer: {
         flex: 1,
-    }
+        overflow: 'hidden',
+    },
+    buttonInnerContainer: {
+        backgroundColor: Colors.yellow,
+        borderRadius: 4,
+        shadowColor: Colors.black,
+        shadowOffset: {width: 0, height: 2},
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+    text: {
+        color: Colors.white,
+        fontSize: 17,
+        fontWeight: 'bold',
+        padding: 10,
+        textAlign: 'center',
+    },
+    pressed: {
+        opacity: 0.75,
+        backgroundColor: Colors.green,
+    },
 });
